ff3f4af1e333e59703e49eef5c566d68616733e2
Rajeshh <rajesh@gmail.com>
2026-04-30 22:38:32 +0530

    widgets fixed

--- toDo/forms.py
+++ toDo/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from toDo.models import Todo
+from todos.models import Todo
 
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
-        fields = ['title','description','due_date','priority','status']
-    
+        fields = ['title', 'description', 'due_date', 'priority', 'status']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'due_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'priority': forms.Select(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+        }
